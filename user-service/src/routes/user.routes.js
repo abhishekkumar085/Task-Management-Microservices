5e4d1c2dcd09d2ec36f8b3225b20a309b228b955
Abhishek Kumar <abhishekkumar08528@gmail.com>
2025-11-07 10:37:56 +0530

    Fixeddd

--- user-service/src/routes/user.routes.js
+++ user-service/src/routes/user.routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const validateRequestBody = require('../middlewares/validate-request-body.js');
 const router = express.Router();
 
 const userController = require('../controllers/user.controller.js');
-const validateRequestBody = require('../middlewares/validate-request-body.js');
 const { createUserSchema } = require('../validators/user.schema.js');
 
 router.post('/users', validateRequestBody(createUserSchema), userController.createUser);
