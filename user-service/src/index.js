50afd9356a0e9c75bdd51b0eb7e83d98ebc54bed
Abhishek Kumar <abhishekkumar08528@gmail.com>
2025-11-10 17:44:16 +0530

    Added docker-compose.yml file

--- user-service/src/index.js
+++ user-service/src/index.js
@@ -3,7 +3,7 @@ const connectDb = require("./config/db.config");
 const userRoutes = require("./routes/user.routes");
 const app = express();
 
-const PORT = process.env.PORT || 8900;
+const PORT = process.env.PORT || 3100;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
